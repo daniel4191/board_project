5ab0942f5b80fffb22e3ea05e38c9f38ec421f29
daniel4191 <daniell8282@naver.com>
2025-10-20 18:05:55 +0900

    models 꼬인거 다시 풀어내고, post, comment, image 추가

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
 
-from .models import BasicPost
+from .models import Post, Comment, PostImage
+
 # Register your models here.
-@admin.register(BasicPost)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+    
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        CommentInline,
+    ]
+    list_display = [
+        "id",
+        "content"
+    ]
+    
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "photo"
+    ]
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "content"
+    ]
--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from users.models import User
 # Create your models here.
+"""
+내가 작성한 post 클래스
 class BasicPost(models.Model):
     title = models.CharField("제목",max_length=100)
     content = models.TextField("내용")
@@ -10,6 +12,8 @@ class BasicPost(models.Model):
     
     def __str__(self):
         return self.title
+"""
+
     
 class CommentPost(models.Model):
     comment = models.TextField("댓글내용")
@@ -18,3 +22,31 @@ class CommentPost(models.Model):
     
     def __str__(self):
         return self.comment
+    
+    
+class Post(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        verbose_name="작성자",
+        on_delete=models.CASCADE
+    )
+    content = models.TextField("내용")
+    created = models.DateTimeField("생성일시", auto_now_add=True)
+    
+class PostImage(models.Model):
+    post = models.ForeignKey(
+        Post,
+        verbose_name="포스트",
+        on_delete=models.CASCADE
+    )
+    photo = models.ImageField("사진", upload_to="post")
+    
+class Comment(models.Model):
+    user = models.ForeignKey(
+        "users.User",
+        verbose_name="작성자",
+        on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(Post, verbose_name="포스트", on_delete=models.CASCADE)
+    content = models.TextField("내용")
+    created = models.DateTimeField("생성일시", auto_now_add=True)
